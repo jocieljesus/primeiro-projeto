d6cce389170864e786e70bacafea310553236958
jocieljesus <jocielalvesdejesus@gmail.com>
2026-06-12 21:25:23 -0300

    Aprendendo variáveis e imprimir na tela

---
+++ programa01.py
@@ -0,0 +1,12 @@
+nome = "Debora"             # str - String
+ultimo_nome = "Samara"      # str - String
+idade = 23                  # int - inteiro
+altura = 1.62               # float - ponto flutuante
+maior_de_idade = True       # bool - booleano : True/False
+
+print("Olá ", nome, ultimo_nome, "você tem", idade, "anos, sua altura é ",altura, "voce é maior de idade? ", maior_de_idade, "!" )
+
+print(f"Olá {nome} {ultimo_nome} você tem {idade} anos, sua altura é {altura}, você é maior de idade? {maior_de_idade}!")
+
+
+

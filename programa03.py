22609e2dc5df1fab83c45bff97f9eff90602e9cc
jocieljesus <jocielalvesdejesus@gmail.com>
2026-06-15 21:24:37 -0300

    entrada de dados pelo terminal e operaçoes matemáticas

---
+++ programa03.py
@@ -0,0 +1,28 @@
+# Calculadora python
+
+num1 = int(input("Digite o primeiro numero: "))
+
+num2 = int(input("Digite o segundo número: "))
+
+soma = num1 + num2
+print(f"A soma de {num1} e {num2} é igual a = {soma}")
+
+subtracao =  num1 - num2
+print(f"A subtracao de {num1} e {num2} é igual a = {subtracao}")
+
+multiplicacao = num1 * num2
+print(f"A multiplicacao de {num1} e {num2} é igual a = {multiplicacao}")
+
+divisao = num1 / num2
+print(f"A divisao de {num1} e {num2} é igual a = {divisao:.2f}")
+
+divisao_inteira = num1//num2
+print(f"A divisao_inteira de {num1} e {num2} é igual a = {divisao_inteira}")
+
+resto_divisao = num1 % num2
+print(f"A resto_divisao de {num1} e {num2} é igual a = {resto_divisao}")
+
+potenciacao = num1 ** num2
+print(f"A potenciacao de {num1} e {num2} é igual a = {potenciacao}")
+
+
